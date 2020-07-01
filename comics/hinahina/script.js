3e952d15c1b382fdafda38d91ddccafaf745fe84
Joyce <hellojoycerice@gmail.com>
2020-07-01 12:19:36 -0400

    adds final student work

--- comics/hinahina/script.js
+++ comics/hinahina/script.js
@@ -1,4 +1,7 @@
 var glower = $('#mainContainer');
+
+glower.toggleClass('active');
+
 window.setInterval(function() {  
     glower.toggleClass('active');
 }, 5000);
--- comics/lucas/script.js
+++ comics/lucas/script.js
@@ -1,56 +1,73 @@
-//Jquery for First Draft
-
 $(document).ready(function(){
     //declare variable for soundclip
-    let thudClipElement =  document.getElementById("thudSound");
-    let chairClipElement = document.getElementById("chairSound");
-    let drawerClipElement = document.getElementById("drawerSound");
+    let downfallElement =  document.getElementById("music1");
+    let officeElement =  document.getElementById("music2");
+    let stablesElement =  document.getElementById("music3");
+    let studyElement = document.getElementById("music4");
     
     //turning the sound on and off on click
     $("#soundControl1").click(function(){
-       if (thudClipElement.paused) {
-           $("#thudSound").trigger("play");
-           $("#soundControl1").attr("src", "assets/speakerOn.png")
+       if (downfallElement.paused) {
+           $("#music1").trigger("play");
+           $("#soundControl1").attr("src", "assets/speakerOn.png");
+                $("#music2").trigger("pause");
+                $("#soundControl2").attr("src", "assets/speakerOff.png");
+                $("#music3").trigger("pause");
+                $("#soundControl3").attr("src", "assets/speakerOff.png");
+                $("#music4").trigger("pause");
+                $("#soundControl4").attr("src", "assets/speakerOff.png"); 
        } else {
-           $("#thudSound").trigger("pause");
-           $("#soundControl1").attr("src", "assets/speakerOff.png") 
+           $("#music1").trigger("pause");
+           $("#soundControl1").attr("src", "assets/speakerOff.png"); 
        }
     });
-    //when the Thud Clip Ends, turn speaker icon to off.
-    thudClipElement.onended = function(){
-            $("#soundControl1").attr("src", "assets/speakerOff.png")
-    };
     
-    
-    
-    $("#soundControl2").click(function(){
-       if (chairClipElement.paused) {
-           $("#chairSound").trigger("play");
-           $("#soundControl2").attr("src", "assets/speakerOn.png")
+        $("#soundControl2").click(function(){
+       if (officeElement.paused) {
+           $("#music2").trigger("play");
+           $("#soundControl2").attr("src", "assets/speakerOn.png");
+                $("#music1").trigger("pause");
+                $("#soundControl1").attr("src", "assets/speakerOff.png");
+                $("#music3").trigger("pause");
+                $("#soundControl3").attr("src", "assets/speakerOff.png");
+                $("#music4").trigger("pause");
+                $("#soundControl4").attr("src", "assets/speakerOff.png"); 
        } else {
-           $("#chairSound").trigger("pause");
-           $("#soundControl2").attr("src", "assets/speakerOff.png") 
+           $("#music2").trigger("pause");
+           $("#soundControl2").attr("src", "assets/speakerOff.png"); 
        }
     });
-    //when the Chair Clip Ends, turn speaker icon to off.
-    chairClipElement.onended = function(){
-            $("#soundControl2").attr("src", "assets/speakerOff.png")
-    };
     
-    
-    //Drawer Sound
         $("#soundControl3").click(function(){
-       if (drawerClipElement.paused) {
-           $("#drawerSound").trigger("play");
-           $("#soundControl3").attr("src", "assets/speakerOn.png")
+       if (stablesElement.paused) {
+            $("#music3").trigger("play");
+            $("#soundControl3").attr("src", "assets/speakerOn.png");
+                $("#music1").trigger("pause");
+                $("#soundControl1").attr("src", "assets/speakerOff.png");
+                $("#music2").trigger("pause");
+                $("#soundControl2").attr("src", "assets/speakerOff.png");
+                $("#music4").trigger("pause");
+                $("#soundControl4").attr("src", "assets/speakerOff.png"); 
        } else {
-           $("#drawerSound").trigger("pause");
-           $("#soundControl3").attr("src", "assets/speakerOff.png") 
+           $("#music3").trigger("pause");
+           $("#soundControl3").attr("src", "assets/speakerOff.png"); 
        }
     });
-    //when the Drawer Clip Ends, turn speaker icon to off.
-    drawerClipElement.onended = function(){
-            $("#soundControl3").attr("src", "assets/speakerOff.png")
-    };
+
+        $("#soundControl4").click(function(){
+       if (studyElement.paused) {
+            $("#music4").trigger("play");
+            $("#soundControl4").attr("src", "assets/speakerOn.png");
+                $("#music1").trigger("pause");
+                $("#soundControl1").attr("src", "assets/speakerOff.png");
+                $("#music2").trigger("pause");
+                $("#soundControl2").attr("src", "assets/speakerOff.png");
+                $("#music3").trigger("pause");
+                $("#soundControl3").attr("src", "assets/speakerOff.png"); 
+       } else {
+           $("#music4").trigger("pause");
+           $("#soundControl4").attr("src", "assets/speakerOff.png"); 
+       }
+    });
     
 });
